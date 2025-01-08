57b0d712773db878bc67ba355d03b7f4cee8ef51
Armiron Kurbalaj <armironkurbalaj@gmail.com>
2025-01-08 19:49:25 +0100

    Refactor chat screen to use StreamBuilder for real-time message updates

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -7,25 +7,28 @@ class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(
-        itemBuilder: (context, index) => Container(
-          padding: const EdgeInsets.all(8),
-          child: const Text('Hello'),
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          FirebaseFirestore.instance
-              .collection('chats/ra4anI7XuHGPyMKElc8w/messages')
-              .snapshots()
-              .listen(
-            (data) {
-              data.docs.forEach((doc) {
-                print(doc['text']);
-              });
-            },
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance
+            .collection('chats/ra4anI7XuHGPyMKElc8w/messages')
+            .snapshots(),
+        builder: (context, streamSnapshot) {
+          if (streamSnapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          final documents = streamSnapshot.data?.docs;
+          return ListView.builder(
+            itemCount: documents?.length,
+            itemBuilder: (context, index) => Container(
+              padding: const EdgeInsets.all(8),
+              child: Text(documents![index]['text']),
+            ),
           );
         },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
         child: const Icon(Icons.add),
       ),
     );
